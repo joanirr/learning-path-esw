2c63cc8f9e8cdf3dec3e268caf17e6c7a4835a2e
joanirr <joanirklod@gmail.com>
2025-11-27 17:42:18 -0300

    add js files

---
+++ programacao_javascript/calculadora_imc/script.js
@@ -0,0 +1,15 @@
+const imc = (peso,altura) => peso / (altura * altura);
+const situacao = (valor_imc) => {
+    if (valor_imc < 18.5)
+        return "Abaixo do peso";
+    else if (valor_imc < 25)
+        return "Peso ideal, Parabéns!";
+    else if (valor_imc < 30)
+        return "Levemente acima do peso";
+    else if (valor_imc < 35)
+        return "Obesidade grau I";
+    else if (valor_imc < 40)
+        return "Obesidade grau II (severa)";
+    else
+        return "Obesidade grau III (mórbida)";
+}
